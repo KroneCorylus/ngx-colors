128b6c050b3dcf6e9a737a2148238f8d64981cae
KroneCorylus <ahustinkrone@gmail.com>
2022-02-01 23:55:24 -0300

    update api docs

--- projects/ngx-color-examples/src/app/const/api.ts
+++ projects/ngx-color-examples/src/app/const/api.ts
@@ -1,36 +1,65 @@
 export var api = {
-    directive:{
-        input:[
-            {
-                name:'palette',
-                description: 'Set a custom palette for the color picker. Can recibe an Array of string or NgxColor'
-            },
-            {
-                name:'colorsAnimation',
-                description: 'Set the animation for the color circles.<br>Options: <ul><li>popup</li><li>slide-in</li><ul>'
-            },
-            {
-                name:'format',
-                description: 'Set output format.<br>Options: <ul><li>hex</li><li>rgba</li><li>hsla</li><ul>'
-            },
-            {
-                name:'hideColorPicker',
-                description: 'Hide the option to see the sliders to choose a color'
-            },
-            {
-                name:'hideTextInput',
-                description: 'Hide the text input'
-            }
-        ],
-        output:[
-            {
-                name:'change',
-                description: 'Gets triggered when the color is changed.'
-            },
-            {
-                name:'input',
-                description: 'Gets triggered when the color is changed by the user'
-            }
-        ]
-    }
-}
+  directive: {
+    input: [
+      {
+        name: "palette",
+        description:
+          "Set a custom palette for the color picker. Can recibe an Array of string or NgxColor",
+      },
+      {
+        name: "colorsAnimation",
+        description:
+          "Set the animation for the color circles.<br>Options: <ul><li>popup</li><li>slide-in</li><ul>",
+      },
+      {
+        name: "format",
+        description:
+          "Set output format.<br>Options: <ul><li>hex</li><li>rgba</li><li>hsla</li><ul>",
+      },
+      {
+        name: "hideColorPicker",
+        description: "Hide the option to see the sliders to choose a color",
+      },
+      {
+        name: "hideTextInput",
+        description: "Hide the text input",
+      },
+      {
+        name: "colorPickerControls",
+        description:
+          "Set the controls for the color picker.<br>Options: <ul><li>default</li><li>only-alpha</li><li>no-alpha</li><ul>",
+      },
+      {
+        name: "acceptLabel",
+        description: "Set the label for the accept button",
+      },
+      {
+        name: "cancelLabel",
+        description: "Set the label for the cancel button",
+      },
+      {
+        name: "overlayClass",
+        description: "Set the class for the overlay",
+      },
+      {
+        name: "appendToID",
+        description: "Set the element to append the overlay",
+      },
+    ],
+    output: [
+      {
+        name: "change",
+        description: "Gets triggered when the color is changed.",
+      },
+      {
+        name: "input",
+        description: "Gets triggered when the color is changed by the user",
+      },
+      {
+        name: "slider",
+        description:
+          "Gets triggered when the the alpha, hue or Hue sliders are changed",
+      },
+    ],
+  },
+};

ede6f2d0832e6d9f876d003e234b5777390bd022
Karel Frederix <kfrederix@selligent.com>
2022-02-08 15:04:53 +0100

    restore unintended formatting changes

--- projects/ngx-colors/src/lib/ngx-colors.component.ts
+++ projects/ngx-colors/src/lib/ngx-colors.component.ts
@@ -1,17 +1,11 @@
-import {
-  Component,
-  Host,
-  OnInit,
-  ChangeDetectorRef,
-  OnDestroy,
-} from "@angular/core";
-import { Subscription } from "rxjs";
-import { NgxColorsTriggerDirective } from "./directives/ngx-colors-trigger.directive";
+import { Component, Host, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NgxColorsTriggerDirective } from './directives/ngx-colors-trigger.directive';
 
 @Component({
-  selector: "ngx-colors",
-  templateUrl: "./ngx-colors.component.html",
-  styleUrls: ["./ngx-colors.component.scss"],
+  selector: 'ngx-colors',
+  templateUrl: './ngx-colors.component.html',
+  styleUrls: ['./ngx-colors.component.scss'],
 })
 export class NgxColorsComponent implements OnInit, OnDestroy {
   private triggerDirectiveColorChangeSubscription: Subscription | null = null;

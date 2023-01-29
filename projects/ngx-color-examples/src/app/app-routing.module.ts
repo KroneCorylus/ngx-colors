f9c7f654a485a36bc1e273510c3b9d46ee5086f8
atorres <agustin.torres@rs.com.ar>
2023-01-28 23:10:01 -0300

    upgrade angular

--- projects/ngx-color-examples/src/app/app-routing.module.ts
+++ projects/ngx-color-examples/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

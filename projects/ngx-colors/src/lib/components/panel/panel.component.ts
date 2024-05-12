2a4740495de969d44aa34d525acb5a7a9e811455
KroneCorylus <ahustinkrone@gmail.com>
2024-05-12 13:40:30 -0300

    basic template y styles for panel

--- projects/ngx-colors/src/lib/components/panel/panel.component.ts
+++ projects/ngx-colors/src/lib/components/panel/panel.component.ts
@@ -1,15 +1,21 @@
-import { Component, HostListener } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { defaultColors } from '../../utility/default-colors';
 @Component({
   selector: 'ngx-colors-panel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.scss', '../../shared/shared.scss'],
 })
-export class PanelComponent {
+export class PanelComponent implements OnInit {
   @HostListener('click', ['$event'])
   public onClick(event: any): void {
     event.stopPropagation();
   }
+  public colors: Array<any> = defaultColors;
+  public selected: string = '#9575CD';
+  constructor() {}
+
+  ngOnInit(): void {}
 }

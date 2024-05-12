c52d0e30876a80c68c77b00b22e9bdc890ccfe63
KroneCorylus <ahustinkrone@gmail.com>
2024-05-12 06:33:41 -0300

    init panel component

---
+++ projects/ngx-colors/src/lib/components/panel/panel.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PanelComponent } from './panel.component';
+
+describe('PanelComponent', () => {
+  let component: PanelComponent;
+  let fixture: ComponentFixture<PanelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PanelComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(PanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/ngx-colors/src/lib/components/panel/panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-colors-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.scss', '../../shared/shared.scss'],
+})
+export class PanelComponent {}

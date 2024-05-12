be5dd0b7e42333fa4dc6efbbb641c079ede74b31
KroneCorylus <ahustinkrone@gmail.com>
2024-05-12 07:05:40 -0300

    basic usage for testing

--- projects/ngx-colors-examples/src/app/app.component.ts
+++ projects/ngx-colors-examples/src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgxColorsComponent } from '../../../ngx-colors/src/public-api';
+import {
+  NgxColorsComponent,
+  NgxColorsTriggerDirective,
+} from '../../../ngx-colors/src/public-api';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgxColorsComponent],
+  imports: [
+    RouterOutlet,
+    NgxColorsComponent,
+    NgxColorsTriggerDirective,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ngx-colors-examples';
+  test: string = 'rgba(255,0,255,0.5)';
+  testCtrl: FormControl<string | undefined | null> = new FormControl<
+    string | undefined | null
+  >('rgba(0,255,100,0.9)');
 }

4682cbc4ad47c0d832d813c7dec8ac40c45cde63
KroneCorylus <ahustinkrone@gmail.com>
2024-05-13 04:23:19 -0300

    tests for overlay service

--- projects/ngx-colors/src/lib/services/overlay.service.spec.ts
+++ projects/ngx-colors/src/lib/services/overlay.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
+import { ApplicationRef } from '@angular/core';
 
 import { OverlayService } from './overlay.service';
 
 describe('OverlayService', () => {
   let service: OverlayService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [OverlayService, ApplicationRef],
+    });
     service = TestBed.inject(OverlayService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should append overlay element to body', () => {
+    service.createOverlay(undefined, undefined);
+    let overlay =
+      document.body.getElementsByTagName('ngx-colors-overlay').length;
+    expect(overlay).toBeTruthy();
+  });
+
+  it('should not append another overlay', () => {
+    service.createOverlay(undefined, undefined);
+    service.createOverlay(undefined, undefined);
+    let overlayCount =
+      document.body.getElementsByTagName('ngx-colors-overlay').length;
+    expect(overlayCount).toBeLessThan(2);
+  });
+
+  it('should remove overlay from the DOM', () => {
+    service.createOverlay(undefined, undefined);
+    service.removePanel();
+    let overlay =
+      document.body.getElementsByTagName('ngx-colors-overlay').length;
+    expect(overlay).toBeFalsy();
   });
 });

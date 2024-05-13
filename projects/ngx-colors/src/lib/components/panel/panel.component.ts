72dea54afd4e0c38a95cacc27ebed84aa8d6086b
KroneCorylus <ahustinkrone@gmail.com>
2024-05-13 03:17:36 -0300

    handling palette navigation

--- projects/ngx-colors/src/lib/components/panel/panel.component.ts
+++ projects/ngx-colors/src/lib/components/panel/panel.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { defaultColors } from '../../utility/default-colors';
+import { Color } from '../../models/color';
+import { Palette } from '../../types/palette';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'ngx-colors-panel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.scss', '../../shared/shared.scss'],
 })
@@ -13,9 +17,33 @@ export class PanelComponent implements OnInit {
   public onClick(event: any): void {
     event.stopPropagation();
   }
-  public colors: Array<any> = defaultColors;
+
+  public palette: Palette = {
+    back: undefined,
+    list: defaultColors.map((c) => new Color(c)),
+  };
   public selected: string = '#9575CD';
   constructor() {}
 
   ngOnInit(): void {}
+
+  public onClickColor(color: Color): void {
+    if (color.childs?.length) {
+      this.palette.back = { ...this.palette };
+      this.palette.list = color.childs;
+    } else {
+      this.selectColor(color);
+    }
+  }
+
+  public onClickBack() {
+    if (this.palette.back) {
+      this.palette.list = this.palette.back.list;
+      this.palette.back = this.palette.back.back;
+    }
+  }
+
+  private selectColor(color: Color) {
+    this.selected = color.preview;
+  }
 }

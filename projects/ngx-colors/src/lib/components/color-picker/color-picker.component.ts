7f1d69567af73598dc46fc1304fa28ed39b47689
KroneCorylus <ahustinkrone@gmail.com>
2024-05-14 06:11:27 -0300

    init color picker component

---
+++ projects/ngx-colors/src/lib/components/color-picker/color-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-colors-color-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './color-picker.component.html',
+  styleUrl: './color-picker.component.scss'
+})
+export class ColorPickerComponent {
+
+}

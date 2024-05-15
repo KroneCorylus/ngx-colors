a86990e957b85d4413e26cc01fd2cc40f881f09c
KroneCorylus <ahustinkrone@gmail.com>
2024-05-14 21:59:53 -0300

    init slider directive

---
+++ projects/ngx-colors/src/lib/directives/slider.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SliderDirective } from './slider.directive';
+
+describe('SliderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SliderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/ngx-colors/src/lib/directives/slider.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[ngxColorsSlider]',
+  standalone: true
+})
+export class SliderDirective {
+
+  constructor() { }
+
+}

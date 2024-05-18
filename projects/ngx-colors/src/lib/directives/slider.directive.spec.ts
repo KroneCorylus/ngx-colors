3c837d6ddc1ff27cf0e402e373c764a67c56689c
KroneCorylus <ahustinkrone@gmail.com>
2024-05-18 10:55:39 -0300

    init test slider directive

--- projects/ngx-colors/src/lib/directives/slider.directive.spec.ts
+++ projects/ngx-colors/src/lib/directives/slider.directive.spec.ts
@@ -1,8 +1,42 @@
 import { SliderDirective } from './slider.directive';
 
-describe('SliderDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SliderDirective();
-    expect(directive).toBeTruthy();
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: ` <div ngxColorsSlider></div> `,
+})
+class HostComponent {
+  value: string = '#ff00ff';
+}
+
+describe('NgxColorsSliderDirective', () => {
+  let fixture: ComponentFixture<HostComponent>;
+  let elementsWithDirective: Array<DebugElement>;
+  let directives: Array<SliderDirective>;
+  let divs: Array<HTMLDivElement>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HostComponent],
+      imports: [SliderDirective],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HostComponent);
+    fixture.detectChanges();
+    elementsWithDirective = fixture.debugElement.queryAll(
+      By.directive(SliderDirective)
+    );
+    directives = elementsWithDirective.map((de: any) =>
+      de.injector.get(SliderDirective)
+    );
+    divs = elementsWithDirective.map((de: any) =>
+      de.injector.get(SliderDirective)
+    );
+  });
+
+  it('should create', () => {
+    expect(directives.length).toBeTruthy();
   });
 });

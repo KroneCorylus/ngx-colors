47245f88600dc25495e51d619102dd09486e9b1d
KroneCorylus <ahustinkrone@gmail.com>
2024-05-18 08:44:05 -0300

    simplify

--- projects/ngx-colors/src/lib/directives/slider.directive.ts
+++ projects/ngx-colors/src/lib/directives/slider.directive.ts
@@ -2,96 +2,109 @@ import {
   Directive,
   ElementRef,
   EventEmitter,
-  HostListener,
-  Input,
   OnDestroy,
   OnInit,
   Output,
+  NgZone,
+  ContentChild,
 } from '@angular/core';
 import {
   Observable,
   Subject,
-  Subscription,
   distinctUntilChanged,
   fromEvent,
   map,
   merge,
+  startWith,
+  switchMap,
   takeUntil,
-  tap,
+  throttleTime,
 } from 'rxjs';
+import { ThumbComponent } from '../components/thumb/thumb.component';
 
 @Directive({
   selector: '[ngxColorsSlider]',
   standalone: true,
 })
 export class SliderDirective implements OnInit, OnDestroy {
-  constructor(private elRef: ElementRef) {}
-
-  @HostListener('mousedown', ['$event'])
-  @HostListener('touchstart', ['$event'])
-  mouseDown(event: MouseEvent | TouchEvent): void {
-    console.log('mousedown');
-    this.start(event);
-  }
-  @Input() handle: 'circle' | 'oval' = 'circle';
-  @Output()
-  change: EventEmitter<[number, number]> = new EventEmitter<[number, number]>();
+  @Output() change: EventEmitter<[number, number]> = new EventEmitter<
+    [number, number]
+  >();
 
   destroy$: Subject<void> = new Subject<void>();
 
-  stop$: Observable<any> = merge(
-    fromEvent<MouseEvent>(document, 'mouseup').pipe(
-      tap((_) => console.log('mouseup'))
-    ),
-    fromEvent<TouchEvent>(document, 'touchup'),
-    this.destroy$
+  @ContentChild(ThumbComponent) thumb: ThumbComponent | undefined;
+
+  private pointerUp$: Observable<PointerEvent> = merge(
+    fromEvent<PointerEvent>(document, 'pointerup'),
+    fromEvent<PointerEvent>(document, 'pointercancel')
   );
 
-  move$: Observable<[number, number]> = merge(
-    fromEvent<MouseEvent>(document, 'mousemove'),
-    fromEvent<TouchEvent>(document, 'touchmove')
-  ).pipe(
-    map((e) => this.getCoordFromEvent(e)),
-    distinctUntilChanged(
-      ([prevX, prevY], [currX, currY]) => prevX === currX && prevY === currY
-    ),
-    takeUntil(this.stop$)
+  private pointerDown$: Observable<PointerEvent> = fromEvent<PointerEvent>(
+    this.elRef.nativeElement,
+    'pointerdown'
   );
 
-  ngOnInit(): void {}
+  private drag$: Observable<[number, number]> = this.pointerDown$.pipe(
+    switchMap((pointerDownEvent) =>
+      fromEvent<PointerEvent>(document, 'pointermove').pipe(
+        startWith(pointerDownEvent),
+        takeUntil(this.pointerUp$)
+      )
+    ),
+    throttleTime(50),
+    distinctUntilChanged((prev, curr) => {
+      return prev.pageY === curr.pageY && prev.pageX === curr.pageX;
+    }),
+    map((event) => this.getCoordFromEvent(event)),
+    takeUntil(this.destroy$)
+  );
+  constructor(
+    private elRef: ElementRef,
+    private _ngZone: NgZone
+  ) {}
+
+  ngOnInit(): void {
+    this.elRef.nativeElement.style.position = 'relative';
+    this._ngZone.runOutsideAngular(() => {
+      this.drag$.subscribe(([x, y]: [number, number]) => {
+        this.setThumbPosition(x, y);
+        this.change.emit(this.normalize(x, y));
+      });
+    });
+  }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
-  moveSubscription: Subscription = new Subscription();
-
-  private start(event: MouseEvent | TouchEvent): void {
-    this.change.emit(this.getCoordFromEvent(event));
-    if (!this.moveSubscription.closed) {
-      this.moveSubscription.unsubscribe();
+  private setThumbPosition(x: number, y: number): void {
+    if (this.thumb) {
+      if (this.thumb.apparence == 'circle') {
+        this.thumb.elementRef.nativeElement.style.top = y + 'px';
+      }
+      this.thumb.elementRef.nativeElement.style.left = x + 'px';
     }
-    this.move$.subscribe((res) => {
-      console.log('mousemove', res);
-      this.change.emit(res);
-    });
+  }
+  private getCoordFromEvent(event: PointerEvent): [number, number] {
+    const position = this.elRef.nativeElement.getBoundingClientRect();
+    const width = this.elRef.nativeElement.offsetWidth;
+    const height = this.elRef.nativeElement.offsetHeight;
+    const x = Math.max(
+      0,
+      Math.min(event.pageX - position.left - window.scrollX, width)
+    );
+    const y = Math.max(
+      0,
+      Math.min(event.pageY - position.top - window.scrollY, height)
+    );
+    return [x, y];
   }
 
-  private getCoordFromEvent(event: MouseEvent | TouchEvent): [number, number] {
-    const position = this.elRef.nativeElement.getBoundingClientRect();
-    const x =
-      ((event as MouseEvent).pageX !== undefined
-        ? (event as MouseEvent).pageX
-        : (event as TouchEvent).touches[0].pageX) -
-      position.left -
-      window.scrollX; // Using type assertion for performance.
-    const y =
-      ((event as MouseEvent).pageY !== undefined
-        ? (event as MouseEvent).pageY
-        : (event as TouchEvent).touches[0].pageY) -
-      position.top -
-      window.scrollY;
-    return [x, y];
+  private normalize(x: number, y: number): [number, number] {
+    const width = this.elRef.nativeElement.offsetWidth;
+    const height = this.elRef.nativeElement.offsetHeight;
+    return [x / width, y / height];
   }
 }

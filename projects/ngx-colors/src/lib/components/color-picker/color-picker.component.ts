09400666d3d2a5840d03af31f605014fe75478f4
KroneCorylus <ahustinkrone@gmail.com>
2024-05-22 10:02:28 -0300

    updating UI on  slider events

--- projects/ngx-colors/src/lib/components/color-picker/color-picker.component.ts
+++ projects/ngx-colors/src/lib/components/color-picker/color-picker.component.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { ApplicationRef, ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { SliderDirective } from '../../directives/slider.directive';
 import { ThumbComponent } from '../thumb/thumb.component';
 import { Hsva } from '../../models/hsva';
 import { Convert } from '../../utility/convert';
-import { Rgba } from '../../models/rgba';
 
 @Component({
   selector: 'ngx-colors-color-picker',
@@ -15,6 +14,7 @@ import { Rgba } from '../../models/rgba';
 })
 export class ColorPickerComponent {
   public hue: string = 'red';
+  public preview: string = 'red';
   public alphaGradient: { background: string } = {
     background:
       'linear-gradient(90deg, rgba(36,0,0,0) 0%, ' + this.hue + ' 100%)',
@@ -23,17 +23,23 @@ export class ColorPickerComponent {
   constructor(private cdr: ChangeDetectorRef) {}
 
   hueHsva: Hsva = new Hsva(1, 1, 1, 1);
-  value: Hsva = new Hsva(0, 0, 0, 0);
+  value: Hsva = new Hsva(1, 1, 1, 1);
 
   public onChangeCoord(sliderCode: string, coord: [number, number]) {
-    let x: number, y: number;
-    [x, y] = coord;
+    const [x, y] = coord;
     if (sliderCode == 'hue') {
       this.hueHsva.h = x * 360;
-      let rgb: Rgba = Convert.hsva2Rgba(this.hueHsva);
-      this.hue = rgb.toString();
+      this.value.h = x * 360;
+      this.hue = Convert.hsva2Rgba(this.hueHsva).toString();
     }
-
+    if (sliderCode == 'sl') {
+      this.value.s = x;
+      this.value.v = 1 - y;
+    }
+    if (sliderCode == 'alpha') {
+      this.value.a = x;
+    }
+    this.preview = Convert.hsva2Rgba(this.value).toString();
     this.cdr.detectChanges();
   }
 }
